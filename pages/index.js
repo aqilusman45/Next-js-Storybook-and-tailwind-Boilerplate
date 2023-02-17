2cd3e24ac5eaee2def687f4f9921c942864c23d4
aqilusman45 <aqilusman45@gmail.com>
2023-02-17 12:19:34 +0500

    feat; Added Init For boilerplate

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Dummy from "../components/Dummy";
+import Text from "../stories/Text/Text";
+
+export default function Home() {
+  return (
+    <div>
+
+      <div  className='text-blue-600'>
+      Test
+      </div>
+
+      <Text />
+
+      <Dummy />
+    </div>
+  )
+}
---
+++ stories/Text/Text.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Text = () => {
+  return (
+    <div className='bg-green-500'>Text</div>
+  )
+}
+
+export default Text
